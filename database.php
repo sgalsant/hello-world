4885cfbf02367ffac90547ed59c334373d90701e
santiago galván <galvangalvan@gmail.com>
2020-11-29 09:40:28 +0000

    Add files via upload

--- database.php
+++ database.php
@@ -14,11 +14,25 @@ try {
 }
 echo "<br>";
 
-$sql = 'SELECT firstName, jobTitle FROM employees ORDER BY firstName';
+$sql = 'SELECT employeeNumber, firstName, jobTitle FROM employees ORDER BY employeeNumber';
 
+$maxNumber = 0;
 echo "<table border='1'>";
-echo "<tr><th>first name</th><th>job title</th></tr>";
+echo "<tr><th>number</th><th>first name</th><th>job title</th></tr>";
 foreach ($conn->query($sql) as $row) {
-    echo "<tr><td>" . $row['firstName'] . "</td><td>" . $row['jobTitle'] . "</td></tr>";
+    echo "<tr><td>" .  $row['employeeNumber'] . "</td><td>" . $row['firstName'] . "</td><td>" . $row['jobTitle'] . "</td></tr>";
+    if ($maxNumber < $row['employeeNumber']) {
+    	$maxNumber = $row['employeeNumber'];
+    }
 }
 echo "</table>";
+
+
+$sql = "insert  into `employees`(`employeeNumber`,`lastName`,`firstName`,`extension`,`email`,`officeCode`,`reportsTo`,`jobTitle`) values (".($maxNumber+1).",'Nuevo','Nuevo','x5800','dmurphy@classicmodelcars.com','1',NULL,'President')";
+
+
+if ($conn->query($sql) ) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
